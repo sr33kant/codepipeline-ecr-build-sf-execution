09c47d71dc6784d634161e9ea29706c6cbb0cdff
sr33kant <sreekant.kondapaneni@hotmail.com>
2021-11-07 12:56:27 -0500

    Update wsgi.py

--- container/decision_trees/wsgi.py
+++ container/decision_trees/wsgi.py
@@ -3,5 +3,4 @@ import predictor as myapp
 # This is just a simple wrapper for gunicorn to find your app.
 # If you want to change the algorithm file, simply change "predictor" above to the
 # new file.
-#test to dploy
 app = myapp.app
